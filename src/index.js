6fdd78a79ca47a662d21e87e7f5840e9559628a7
ummidimanikanta <mummidi@ghsltechnologies.com>
2020-10-04 22:11:08 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {ProductProvider} from './context';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ProductProvider>
+    <Router>
     <App />
-  </React.StrictMode>,
+    </Router>
+  </ProductProvider>,
   document.getElementById('root')
 );
 
